e91f8e9bf60449d6bb6ef935214472c78c7d0fbe
François Colas <francois.colas@ledger.fr>
2020-12-14 16:44:29 +0100

    Fix bound check when format memo

--- src/transaction/utils.c
+++ src/transaction/utils.c
@@ -19,6 +19,8 @@
 #include <stdbool.h>  // bool
 #include <string.h>   // memmove
 
+#include "types.h"
+
 bool transaction_utils_check_encoding(const uint8_t *memo, uint64_t memo_len) {
     for (uint64_t i = 0; i < memo_len; i++) {
         if (memo[i] > 0x7F) {
@@ -33,7 +35,7 @@ bool transaction_utils_format_memo(const uint8_t *memo,
                                    uint64_t memo_len,
                                    char *dst,
                                    uint64_t dst_len) {
-    if (dst_len < memo_len + 1) {
+    if (memo_len > MAX_MEMO_LEN || dst_len < memo_len + 1) {
         return false;
     }
 
