3a4cfc9c388355ca628c0739bc67080803ee67bd
Sarah GLINER <sarah.gliner@ledger.fr>
2023-02-08 18:06:40 +0100

    app_main.c: add new file now that main.c moved to sdk

---
+++ src/app_main.c
@@ -0,0 +1,96 @@
+/*****************************************************************************
+ *   Ledger App Boilerplate.
+ *   (c) 2020 Ledger SAS.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************/
+
+#include <stdint.h>  // uint*_t
+#include <string.h>  // memset, explicit_bzero
+
+#include "os.h"
+#include "ux.h"
+
+#include "types.h"
+#include "globals.h"
+#include "io.h"
+#include "sw.h"
+#include "ui/menu.h"
+#include "apdu/parser.h"
+#include "apdu/dispatcher.h"
+
+global_ctx_t G_context;
+
+/**
+ * Handle APDU command received and send back APDU response using handlers.
+ */
+void app_main() {
+    // Length of APDU command received in G_io_apdu_buffer
+    int input_len = 0;
+    // Structured APDU command
+    command_t cmd;
+
+    io_init();
+
+    ui_menu_main();
+
+    // Reset context
+    explicit_bzero(&G_context, sizeof(G_context));
+
+    for (;;) {
+        BEGIN_TRY {
+            TRY {
+                // Reset structured APDU command
+                memset(&cmd, 0, sizeof(cmd));
+
+                // Receive command bytes in G_io_apdu_buffer
+                if ((input_len = io_recv_command()) < 0) {
+                    CLOSE_TRY;
+                    return;
+                }
+
+                // Parse APDU command from G_io_apdu_buffer
+                if (!apdu_parser(&cmd, G_io_apdu_buffer, input_len)) {
+                    PRINTF("=> /!\\ BAD LENGTH: %.*H\n", input_len, G_io_apdu_buffer);
+                    io_send_sw(SW_WRONG_DATA_LENGTH);
+                    CLOSE_TRY;
+                    continue;
+                }
+
+                PRINTF("=> CLA=%02X | INS=%02X | P1=%02X | P2=%02X | Lc=%02X | CData=%.*H\n",
+                       cmd.cla,
+                       cmd.ins,
+                       cmd.p1,
+                       cmd.p2,
+                       cmd.lc,
+                       cmd.lc,
+                       cmd.data);
+
+                // Dispatch structured APDU command to handler
+                if (apdu_dispatcher(&cmd) < 0) {
+                    CLOSE_TRY;
+                    return;
+                }
+            }
+            CATCH(EXCEPTION_IO_RESET) {
+                THROW(EXCEPTION_IO_RESET);
+            }
+            CATCH_OTHER(e) {
+                io_send_sw(e);
+            }
+            FINALLY {
+            }
+            END_TRY;
+        }
+    }
+}
