a3df3d33b97379643aa61c4dad79f07a639c0472
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2023-03-09 10:04:35 +0100

    update scripts

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -2,30 +2,30 @@ if [ "$1" = "SP" ]
 then
   echo "Build target $1"
   BOLOS_SDK=$NANOSP_SDK make clean
-  BOLOS_SDK=$NANOSP_SDK make
+  BOLOS_SDK=$NANOSP_SDK make -j
 elif [ "$1" = "S" ]
 then
   echo "Build target $1"
   BOLOS_SDK=$NANOS_SDK make clean
-  BOLOS_SDK=$NANOS_SDK make
+  BOLOS_SDK=$NANOS_SDK make -j
 elif [ "$1" = "STAX" ]
 then
   echo "Build target $1"
   BOLOS_SDK=$STAX_SDK make clean
-  BOLOS_SDK=$STAX_SDK make
+  BOLOS_SDK=$STAX_SDK make -j
 elif [ "$1" = "X" ]
 then
   echo "Build target $1"
   BOLOS_SDK=$NANOX_SDK make clean
-  BOLOS_SDK=$NANOX_SDK make
+  BOLOS_SDK=$NANOX_SDK make -j
 elif [ "$1" = "all" ]
 then
   echo "Build target $1"
   BOLOS_SDK=$NANOX_SDK make clean
-  BOLOS_SDK=$NANOX_SDK make
-  BOLOS_SDK=$NANOS_SDK make
-  BOLOS_SDK=$NANOSP_SDK make
-  BOLOS_SDK=$STAX_SDK make
+  BOLOS_SDK=$NANOX_SDK make -j
+  BOLOS_SDK=$NANOS_SDK make -j
+  BOLOS_SDK=$NANOSP_SDK make -j
+  BOLOS_SDK=$STAX_SDK make -j
 else
   echo "Target is wrong, try again."
 fi
