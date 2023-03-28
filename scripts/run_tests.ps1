6144b88d2a69c165fbb8e9445f728f92253c5149
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2023-03-28 10:51:58 +0200

    [scripts] update the script to launch tests on windows

--- scripts/run_tests.ps1
+++ scripts/run_tests.ps1
@@ -2,4 +2,14 @@ param(
 [string]$arg1
 )
 
-docker run --rm -ti  -v "$(pwd):/app:rw" --privileged -v "/dev/bus/usb:/dev/bus/usb" -e DISPLAY=10.0.55.135:0 app-tester:latest sh -c "cd app && pytest -v --display --tb=short tests/ --cov ragger --cov-report xml --device $arg1"
+$pkg_dependencies="apk add gcc musl-dev && apk add python3-dev && pip install pysha3"
+
+# with display
+docker run --rm -ti  -v "$(pwd):/app:rw" --privileged -v "/dev/bus/usb:/dev/bus/usb" -e DISPLAY=10.0.55.135:0 app-tester:latest sh -c "$pkg_dependencies && cd app && pytest -v --display --tb=short tests/ --cov ragger --cov-report xml --device $arg1"
+
+# without display
+# docker run --rm -ti  -v "$(pwd):/app:rw" --privileged -v "/dev/bus/usb:/dev/bus/usb" -e DISPLAY=10.0.55.135:0 app-tester:latest sh -c "$pkg_dependencies && cd app && pytest -v --tb=short tests/ --cov ragger --cov-report xml --device $arg1"
+
+
+
+
