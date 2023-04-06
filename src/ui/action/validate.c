64cc5422923e15861ca84a8e675847de65989861
Xavier Chapron <xavier.chapron@ledger.fr>
2023-04-06 16:18:11 +0200

    wip: src: Implement and use crypto_safe functions

--- src/ui/action/validate.c
+++ src/ui/action/validate.c
@@ -17,10 +17,11 @@
 
 #include <stdbool.h>  // bool
 
+#include "crypto_helpers.h"
+
 #include "validate.h"
 #include "../menu.h"
 #include "../../sw.h"
-#include "../../crypto.h"
 #include "../../globals.h"
 #include "../../helper/send_response.h"
 
@@ -31,6 +32,33 @@ void validate_pubkey(bool choice) {
         io_send_sw(SW_DENY);
     }
 }
+
+static int crypto_sign_message(void) {
+    uint32_t info = 0;
+    size_t sig_len = sizeof(G_context.tx_info.signature);
+
+    cx_err_t error = bip32_derive_ecdsa_sign_hash_256(CX_CURVE_256K1,
+                                                      G_context.bip32_path,
+                                                      G_context.bip32_path_len,
+                                                      CX_RND_RFC6979 | CX_LAST,
+                                                      CX_SHA256,
+                                                      G_context.tx_info.m_hash,
+                                                      sizeof(G_context.tx_info.m_hash),
+                                                      G_context.tx_info.signature,
+                                                      &sig_len,
+                                                      &info);
+    if (error != CX_OK) {
+        return -1;
+    }
+
+    PRINTF("Signature: %.*H\n", sig_len, G_context.tx_info.signature);
+
+    G_context.tx_info.signature_len = sig_len;
+    G_context.tx_info.v = (uint8_t)(info & CX_ECCINFO_PARITY_ODD);
+
+    return 0;
+}
+
 
 void validate_transaction(bool choice) {
     if (choice) {
