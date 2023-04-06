1c64a75eb9142921d07a9141ffd1d7dea09bab41
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2023-04-06 18:57:56 +0200

    update build and load scripts to be able to run the scripts outside the app folder

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,1 +1,2 @@
-docker run --rm -ti  -v "$(realpath .):/app" --privileged -v "/dev/bus/usb:/dev/bus/usb" ghcr.io/ledgerhq/ledger-app-builder/ledger-app-builder-lite:latest bash -c "sh scripts/load_setup.sh $1"
+declare -A options=(["SP"]="\$NANOSP_SDK" ["S"]="\$NANOS_SDK" ["X"]="\$NANOX_SDK" ["STAX"]="\$STAX_SDK" )
+docker run --rm -ti  -v "$(realpath .):/app" --privileged -v "/dev/bus/usb:/dev/bus/usb" ghcr.io/ledgerhq/ledger-app-builder/ledger-app-builder-lite:latest bash -c "BOLOS_SDK=${options[$1]} make load"
