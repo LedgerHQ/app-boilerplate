a5f574fd3ac709de44bdc35b9462403853abde6f
Xavier Chapron <xavier.chapron@ledger.fr>
2023-05-09 15:03:16 +0200

    tests: Remove raise_policy changes that might triggers tests collusions

--- tests/test_error_cmd.py
+++ tests/test_error_cmd.py
@@ -1,56 +1,57 @@
-from ragger.backend import RaisePolicy
+import pytest
+
+from ragger.error import ExceptionRAPDU
 from application_client.boilerplate_command_sender import CLA, InsType, P1, P2, Errors
 
 
 # Ensure the app returns an error when a bad CLA is used
 def test_bad_cla(backend):
-    # Disable raising when trying to unpack an error APDU
-    backend.raise_policy = RaisePolicy.RAISE_NOTHING
-    rapdu = backend.exchange(cla=CLA + 1, ins=InsType.GET_VERSION)
-    assert rapdu.status == Errors.SW_CLA_NOT_SUPPORTED
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA + 1, ins=InsType.GET_VERSION)
+    assert e.value.status == Errors.SW_CLA_NOT_SUPPORTED
 
 
 # Ensure the app returns an error when a bad INS is used
 def test_bad_ins(backend):
-    # Disable raising when trying to unpack an error APDU
-    backend.raise_policy = RaisePolicy.RAISE_NOTHING
-    rapdu = backend.exchange(cla=CLA, ins=0xff)
-    assert rapdu.status == Errors.SW_INS_NOT_SUPPORTED
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA, ins=0xff)
+    assert e.value.status == Errors.SW_INS_NOT_SUPPORTED
 
 
 # Ensure the app returns an error when a bad P1 or P2 is used
 def test_wrong_p1p2(backend):
-    # Disable raising when trying to unpack an error APDU
-    backend.raise_policy = RaisePolicy.RAISE_NOTHING
-    rapdu = backend.exchange(cla=CLA, ins=InsType.GET_VERSION, p1=P1.P1_START + 1, p2=P2.P2_LAST)
-    assert rapdu.status == Errors.SW_WRONG_P1P2
-    rapdu = backend.exchange(cla=CLA, ins=InsType.GET_VERSION, p1=P1.P1_START, p2=P2.P2_MORE)
-    assert rapdu.status == Errors.SW_WRONG_P1P2
-    rapdu = backend.exchange(cla=CLA, ins=InsType.GET_APP_NAME, p1=P1.P1_START + 1, p2=P2.P2_LAST)
-    assert rapdu.status == Errors.SW_WRONG_P1P2
-    rapdu = backend.exchange(cla=CLA, ins=InsType.GET_APP_NAME, p1=P1.P1_START, p2=P2.P2_MORE)
-    assert rapdu.status == Errors.SW_WRONG_P1P2
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA, ins=InsType.GET_VERSION, p1=P1.P1_START + 1, p2=P2.P2_LAST)
+    assert e.value.status == Errors.SW_WRONG_P1P2
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA, ins=InsType.GET_VERSION, p1=P1.P1_START, p2=P2.P2_MORE)
+    assert e.value.status == Errors.SW_WRONG_P1P2
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA, ins=InsType.GET_APP_NAME, p1=P1.P1_START + 1, p2=P2.P2_LAST)
+    assert e.value.status == Errors.SW_WRONG_P1P2
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA, ins=InsType.GET_APP_NAME, p1=P1.P1_START, p2=P2.P2_MORE)
+    assert e.value.status == Errors.SW_WRONG_P1P2
 
 
 # Ensure the app returns an error when a bad data length is used
 def test_wrong_data_length(backend):
-    # Disable raising when trying to unpack an error APDU
-    backend.raise_policy = RaisePolicy.RAISE_NOTHING
     # APDUs must be at least 5 bytes: CLA, INS, P1, P2, Lc.
-    rapdu = backend.exchange_raw(b"E0030000")
-    assert rapdu.status == Errors.SW_WRONG_DATA_LENGTH
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange_raw(b"E0030000")
+    assert e.value.status == Errors.SW_WRONG_DATA_LENGTH
     # APDUs advertises a too long length
-    rapdu = backend.exchange_raw(b"E003000005")
-    assert rapdu.status == Errors.SW_WRONG_DATA_LENGTH
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange_raw(b"E003000005")
+    assert e.value.status == Errors.SW_WRONG_DATA_LENGTH
 
 
 # Ensure there is no state confusion when trying wrong APDU sequences
 def test_invalid_state(backend):
-    # Disable raising when trying to unpack an error APDU
-    backend.raise_policy = RaisePolicy.RAISE_NOTHING
-    rapdu = backend.exchange(cla=CLA,
-                             ins=InsType.SIGN_TX,
-                             p1=P1.P1_START + 1, # Try to continue a flow instead of start a new one
-                             p2=P2.P2_MORE,
-                             data=b"abcde") # data is not parsed in this case
-    assert rapdu.status == Errors.SW_BAD_STATE
+    with pytest.raises(ExceptionRAPDU) as e:
+        backend.exchange(cla=CLA,
+                         ins=InsType.SIGN_TX,
+                         p1=P1.P1_START + 1,  # Try to continue a flow instead of start a new one
+                         p2=P2.P2_MORE,
+                         data=b"abcde")  # data is not parsed in this case
+    assert e.value.status == Errors.SW_BAD_STATE
--- tests/test_pubkey_cmd.py
+++ tests/test_pubkey_cmd.py
@@ -1,7 +1,9 @@
+import pytest
+
 from application_client.boilerplate_command_sender import BoilerplateCommandSender, Errors
 from application_client.boilerplate_response_unpacker import unpack_get_public_key_response
 from ragger.bip import calculate_public_key_and_chaincode, CurveChoice
-from ragger.backend import RaisePolicy
+from ragger.error import ExceptionRAPDU
 from ragger.navigator import NavInsID, NavIns
 from utils import ROOT_SCREENSHOT_PATH
 
@@ -55,20 +57,16 @@ def test_get_public_key_confirm_refused(firmware, backend, navigator, test_name)
     path = "m/44'/1'/0'/0/0"
 
     if firmware.device.startswith("nano"):
-        with client.get_public_key_with_confirmation(path=path):
-            # Disable raising when trying to unpack an error APDU
-            backend.raise_policy = RaisePolicy.RAISE_NOTHING
-            navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
-                                                      [NavInsID.BOTH_CLICK],
-                                                      "Reject",
-                                                      ROOT_SCREENSHOT_PATH,
-                                                      test_name)
-
-        response = client.get_async_response()
-
+        with pytest.raises(ExceptionRAPDU) as e:
+            with client.get_public_key_with_confirmation(path=path):
+                navigator.navigate_until_text_and_compare(NavInsID.RIGHT_CLICK,
+                                                          [NavInsID.BOTH_CLICK],
+                                                          "Reject",
+                                                          ROOT_SCREENSHOT_PATH,
+                                                          test_name)
         # Assert that we have received a refusal
-        assert response.status == Errors.SW_DENY
-        assert len(response.data) == 0
+        assert e.value.status == Errors.SW_DENY
+        assert len(e.value.data) == 0
     else:
         instructions_set = [
             [
@@ -82,13 +80,11 @@ def test_get_public_key_confirm_refused(firmware, backend, navigator, test_name)
             ]
         ]
         for i, instructions in enumerate(instructions_set):
-            with client.get_public_key_with_confirmation(path=path):
-                backend.raise_policy = RaisePolicy.RAISE_NOTHING
-                navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
-                                               test_name + f"/part{i}",
-                                               instructions)
-            response = client.get_async_response()
-
+            with pytest.raises(ExceptionRAPDU) as e:
+                with client.get_public_key_with_confirmation(path=path):
+                    navigator.navigate_and_compare(ROOT_SCREENSHOT_PATH,
+                                                   test_name + f"/part{i}",
+                                                   instructions)
             # Assert that we have received a refusal
-            assert response.status == Errors.SW_DENY
-            assert len(response.data) == 0
+            assert e.value.status == Errors.SW_DENY
+            assert len(e.value.data) == 0
