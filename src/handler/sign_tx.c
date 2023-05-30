55dd1bff90087cd07c7dfa0d05876ed7baaafebc
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2023-05-30 14:49:05 +0200

    code format fix

--- src/handler/sign_tx.c
+++ src/handler/sign_tx.c
@@ -83,8 +83,7 @@ int handler_sign_tx(buffer_t *cdata, uint8_t chunk, bool more) {
 
             cx_sha3_t keccak256;
 
-            if(cx_keccak_init_no_throw(&keccak256, 256) != CX_OK)
-            {
+            if(cx_keccak_init_no_throw(&keccak256, 256) != CX_OK) {
                 return io_send_sw(SW_TX_HASH_FAIL);
             }
 
@@ -93,8 +92,7 @@ int handler_sign_tx(buffer_t *cdata, uint8_t chunk, bool more) {
                                 G_context.tx_info.raw_tx,
                                 G_context.tx_info.raw_tx_len,
                                 G_context.tx_info.m_hash,
-                                sizeof(G_context.tx_info.m_hash)) != CX_OK)
-            {
+                                sizeof(G_context.tx_info.m_hash)) != CX_OK) {
                 return io_send_sw(SW_TX_HASH_FAIL);
             } 
 
