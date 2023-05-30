bc37e82c2627f0d3fd1263050a1ed0b9eee14cc7
tdejoigny-ledger <thibault.de-joigny@ledger.fr>
2023-05-30 14:37:31 +0200

    Replace throwable functions with "no throw" functions

--- src/address.c
+++ src/address.c
@@ -35,8 +35,15 @@ bool address_from_pubkey(const uint8_t public_key[static 65], uint8_t *out, size
         return false;
     }
 
-    cx_keccak_init(&keccak256, 256);
-    cx_hash((cx_hash_t *) &keccak256, CX_LAST, public_key + 1, 64, address, sizeof(address));
+    if(cx_keccak_init_no_throw(&keccak256, 256) != CX_OK)
+    {
+        return false;
+    }
+
+    if(cx_hash_no_throw((cx_hash_t *) &keccak256, CX_LAST, public_key + 1, 64, address, sizeof(address)) != CX_OK)
+    {
+        return false;
+    }
 
     memmove(out, address + sizeof(address) - ADDRESS_LEN, ADDRESS_LEN);
 
