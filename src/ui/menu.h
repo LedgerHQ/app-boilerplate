8c2fbb584431b9cee3e1e4259f360d0a29814736
Xavier Chapron <xavier.chapron@ledger.fr>
2024-02-27 11:57:18 +0100

    src/ui: Update NBGL UI based on new API

--- src/ui/menu.h
+++ src/ui/menu.h
@@ -6,11 +6,6 @@
 void ui_menu_main(void);
 
 /**
- * Show settings menu (App version, developer name and settings).
- */
-void ui_menu_settings(void);
-
-/**
  * Show about submenu (copyright, date).
  */
 void ui_menu_about(void);
