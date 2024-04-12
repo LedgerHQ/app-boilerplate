febda8b47038c290704553992cbd7306253eb5f0
Lucas PASCAL <lucas.pascal@ledger.com>
2024-04-12 10:33:35 +0200

    [update] Tests for flex

--- tests/test_app_mainmenu.py
+++ tests/test_app_mainmenu.py
@@ -1,22 +1,33 @@
+from ragger.firmware import Firmware
 from ragger.navigator import NavInsID, NavIns
 
 
 # In this test we check the behavior of the device main menu
 def test_app_mainmenu(firmware, navigator, test_name, default_screenshot_path):
     # Navigate in the main menu
-    if firmware.device.startswith("nano"):
+    if firmware.is_nano:
         instructions = [
             NavInsID.RIGHT_CLICK,
             NavInsID.RIGHT_CLICK,
             NavInsID.RIGHT_CLICK
         ]
-    else:
+    elif firmware is Firmware.STAX:
         instructions = [
             NavInsID.USE_CASE_HOME_SETTINGS,
             NavIns(NavInsID.TOUCH, (200, 113)),
             NavIns(NavInsID.TOUCH, (200, 261)),
             NavInsID.USE_CASE_CHOICE_CONFIRM,
             NavIns(NavInsID.TOUCH, (200, 261)),
+            NavInsID.USE_CASE_SETTINGS_NEXT,
+            NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT
+        ]
+    elif firmware is Firmware.FLEX:
+        instructions = [
+            NavInsID.USE_CASE_HOME_SETTINGS,
+            NavIns(NavInsID.TOUCH, (200, 113)),
+            NavIns(NavInsID.TOUCH, (200, 300)),
+            NavInsID.USE_CASE_CHOICE_CONFIRM,
+            NavIns(NavInsID.TOUCH, (200, 300)),
             NavInsID.USE_CASE_SETTINGS_NEXT,
             NavInsID.USE_CASE_SETTINGS_MULTI_PAGE_EXIT
         ]
