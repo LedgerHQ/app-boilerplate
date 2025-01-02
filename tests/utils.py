c261768edf3f8ac6d787ef5ee412cc47a05d87ee
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-01-02 18:14:08 +0100

    Update dynamic check of AppName and AppVersion

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import List
+import re
 from hashlib import sha256
 from sha3 import keccak_256
 
@@ -17,3 +20,54 @@ def check_signature_validity(public_key: bytes, signature: bytes, message: bytes
                      data=message,
                      hashfunc=keccak_256,
                      sigdecode=sigdecode_der)
+
+
+def verify_name(name: str) -> None:
+    """Verify the app name, based on defines in Makefile
+
+    Args:
+        name (str): Name to be checked
+    """
+
+    name_str = ""
+    lines = _read_makefile()
+    name_re = re.compile(r"^APPNAME\s?=\s?\"?(?P<val>\w+)\"?", re.I)
+    for line in lines:
+        info = name_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            name_str = dinfo["val"]
+    assert name == name_str
+
+
+def verify_version(version: str) -> None:
+    """Verify the app version, based on defines in Makefile
+
+    Args:
+        Version (str): Version to be checked
+    """
+
+    vers_dict = {}
+    vers_str = ""
+    lines = _read_makefile()
+    version_re = re.compile(r"^APPVERSION_(?P<part>\w)\s?=\s?(?P<val>\d*)", re.I)
+    for line in lines:
+        info = version_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            vers_dict[dinfo["part"]] = dinfo["val"]
+    try:
+        vers_str = f"{vers_dict['M']}.{vers_dict['N']}.{vers_dict['P']}"
+    except KeyError:
+        pass
+    assert version == vers_str
+
+
+def _read_makefile() -> List[str]:
+    """Read lines from the parent Makefile """
+
+    parent = Path(__file__).parent.parent.resolve()
+    makefile = f"{parent}/Makefile"
+    with open(makefile, "r", encoding="utf-8") as f_p:
+        lines = f_p.readlines()
+    return lines
