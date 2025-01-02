1f713d9d3746444510a3e4133f0e578585f8dfb7
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-01-02 18:14:08 +0100

    Update dynamic check of AppName and AppVersion

--- tests/test_appname_cmd.py
+++ tests/test_appname_cmd.py
@@ -1,6 +1,8 @@
 from application_client.boilerplate_command_sender import BoilerplateCommandSender
 from application_client.boilerplate_response_unpacker import unpack_get_app_name_response
 
+from utils import verify_name
+
 
 # In this test we check that the GET_APP_NAME replies the application name
 def test_app_name(backend):
@@ -9,4 +11,4 @@ def test_app_name(backend):
     # Send the GET_APP_NAME instruction to the app
     response = client.get_app_name()
     # Assert that we have received the correct appname
-    assert unpack_get_app_name_response(response.data) == "Boilerplate"
+    verify_name(unpack_get_app_name_response(response.data))
--- tests/test_version_cmd.py
+++ tests/test_version_cmd.py
@@ -1,10 +1,7 @@
 from application_client.boilerplate_command_sender import BoilerplateCommandSender
 from application_client.boilerplate_response_unpacker import unpack_get_version_response
 
-# Taken from the Makefile, to update every time the Makefile version is bumped
-MAJOR = 2
-MINOR = 1
-PATCH = 0
+from utils import verify_version
 
 # In this test we check the behavior of the device when asked to provide the app version
 def test_version(backend):
@@ -13,4 +10,5 @@ def test_version(backend):
     # Send the GET_VERSION instruction
     rapdu = client.get_version()
     # Use an helper to parse the response, assert the values
-    assert unpack_get_version_response(rapdu.data) == (MAJOR, MINOR, PATCH)
+    MAJOR, MINOR, PATCH = unpack_get_version_response(rapdu.data)
+    verify_version(f"{MAJOR}.{MINOR}.{PATCH}")
