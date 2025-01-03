bf18b7f59d0ffa4b3abd68d23217759003a8a8f2
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-01-03 17:34:36 +0100

    Linter

--- src/ui/action/validate.c
+++ src/ui/action/validate.c
@@ -54,7 +54,7 @@ static int crypto_sign_message(void) {
     PRINTF("Signature: %.*H\n", sig_len, G_context.tx_info.signature);
 
     G_context.tx_info.signature_len = sig_len;
-    G_context.tx_info.v = (uint8_t)(info & CX_ECCINFO_PARITY_ODD);
+    G_context.tx_info.v = (uint8_t) (info & CX_ECCINFO_PARITY_ODD);
 
     return 0;
 }
