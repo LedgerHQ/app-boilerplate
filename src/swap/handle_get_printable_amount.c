c7a38dd4d789ba63af123bfececaa33837668df9
GroM <jerome.caporossi@ledger.fr>
2025-02-20 18:26:13 +0100

    Handle check_address and get_printable_amount

---
+++ src/swap/handle_get_printable_amount.c
@@ -0,0 +1,39 @@
+#ifdef HAVE_SWAP
+#include "swap.h"
+#include "buffer.h"
+#include "constants.h"
+#include "format.h"
+#include "os.h"
+
+#include <string.h>
+#include <stdio.h>
+
+void swap_handle_get_printable_amount(get_printable_amount_parameters_t* params) {
+    PRINTF("Inside swap_handle_get_printable_amount\n");
+
+    PRINTF("Amount: %.*H\n", params->amount_length, params->amount);
+
+    uint64_t value = 0;
+    char amount[30] = {0};
+
+    buffer_t buf = {
+        .ptr = params->amount,
+        .size = params->amount_length,
+        .offset = 0
+    };
+
+    /* convert the buffer params->amount into a integer */
+    uint8_t byte;
+    while (buf.offset < buf.size) {    
+        buffer_read_u8(&buf, &byte);
+        value = (value << 8) | byte;
+    }
+    
+    format_fpu64(amount, sizeof(amount), value, EXPONENT_SMALLEST_UNIT);
+
+    PRINTF("Formatted amount: %s\n", amount);
+
+    memset(params->printable_amount, 0, sizeof(params->printable_amount));
+    snprintf(params->printable_amount, sizeof(params->printable_amount), "BOL %.*s", sizeof(amount), amount);
+}
+#endif // HAVE_SWAP
---
+++ src/swap/handle_swap_sign_transaction.c
@@ -0,0 +1,7 @@
+#ifdef HAVE_SWAP
+#include "swap.h"
+#include <stdint.h>
+bool swap_copy_transaction_parameters(create_transaction_parameters_t* params){
+    return true;
+}
+#endif // HAVE_SWAP
