cd602cd3a6e91beb73899f7c5dae65f4db6a4d47
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2025-06-03 09:18:11 +0200

    Remove old useless nanoS 16px icons

--- src/ui/display.h
+++ src/ui/display.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>  // bool
 
 #if defined(TARGET_NANOX) || defined(TARGET_NANOS2)
-#define ICON_APP_BOILERPLATE C_app_boilerplate_16px
+#define ICON_APP_BOILERPLATE C_app_boilerplate_14px
 #define ICON_APP_WARNING     C_icon_warning
 #elif defined(TARGET_STAX) || defined(TARGET_FLEX)
 #define ICON_APP_BOILERPLATE C_app_boilerplate_64px
