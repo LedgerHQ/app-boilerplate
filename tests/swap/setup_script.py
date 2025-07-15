2e9026e691a2a70fd323db3a2c3f00386f832cf2
Francois Beutin <francois.beutin@ledger.fr>
2025-07-15 13:38:58 +0200

    Unify ragger lib loading

--- tests/swap/setup_script.py
+++ tests/swap/setup_script.py
@@ -1,37 +1,34 @@
-from git import Repo
 import os
 import subprocess
+from git import Repo
 from pathlib import Path
+
+APP_EXCHANGE_URL = "git@github.com:LedgerHQ/app-exchange.git"
+APP_EXCHANGE_DIR = ".test_dependencies/main/app-exchange/"
+APP_EXCHANGE_CLONE_DIR = ".test_dependencies/app-exchange/"
+
+APP_ETHEREUM_URL = "git@github.com:LedgerHQ/app-ethereum.git"
+APP_ETHEREUM_DIR = ".test_dependencies/libraries/app-ethereum/"
+APP_ETHEREUM_CLONE_DIR = ".test_dependencies/app-ethereum/"
 
 DEVICES_CONF = {
     "nanos+": {
-        "name": "nanos2",
         "sdk": "NANOSP_SDK",
         "bin_path": "build/nanos2/bin/",
     },
     "nanox": {
-        "name": "nanox",
         "sdk": "NANOX_SDK",
         "bin_path": "build/nanox/bin/",
     },
     "stax": {
-        "name": "stax",
         "sdk": "STAX_SDK",
         "bin_path": "build/stax/bin/",
     },
     "flex": {
-        "name": "flex",
         "sdk": "FLEX_SDK",
         "bin_path": "build/flex/bin/",
     },
 }
-
-
-APP_EXCHANGE_URL = "git@github.com:LedgerHQ/app-exchange.git"
-APP_EXCHANGE_DIR = "./app-exchange"
-
-APP_ETHEREUM_URL = "git@github.com:LedgerHQ/app-ethereum.git"
-APP_ETHEREUM_DIR = "./app-ethereum"
 
 def run_cmd(cmd: str,
             cwd: Path = Path('.'),
@@ -56,31 +53,42 @@ def run_cmd(cmd: str,
         print(f"[run_cmd] Output:\n{ret.stdout}")
 
     return ret.stdout.strip()
-    
 
-# Build Exchange app for all devices
-# test if the directory already exists
-if not os.path.exists("main_app"):
-    if not os.path.exists(APP_EXCHANGE_DIR):
-        Repo.clone_from(APP_EXCHANGE_URL, APP_EXCHANGE_DIR)
-    os.makedirs("main_app/exchange", exist_ok=True)
-    flags = ' TESTING=1 TEST_PUBLIC_KEY=1 TRUSTED_NAME_TEST_KEY=1 DEBUG=1'
-    run_cmd("make clean", Path(APP_EXCHANGE_DIR))
-    for e in DEVICES_CONF.values():
-        run_cmd("make -j BOLOS_SDK=$"+e["sdk"]+flags, Path(APP_EXCHANGE_DIR))
-        os.makedirs("main_app/exchange/"+e["bin_path"], exist_ok=True)
-        run_cmd("cp "+e["bin_path"]+"app.elf ../main_app/exchange/"+e["bin_path"], Path(APP_EXCHANGE_DIR))
-    run_cmd("rm -rf "+APP_EXCHANGE_DIR)
+def clone_or_pull(repo_url: str, clone_dir: str):
+    git_dir = os.path.join(clone_dir, ".git")
+    if not os.path.exists(git_dir):
+        print(f"Cloning into {clone_dir}")
+        run_cmd(f"rm -rf {clone_dir}")
+        Repo.clone_from(repo_url, clone_dir, recursive=True)
+    else:
+        print(f"Pulling latest changes in {clone_dir}")
+        repo = Repo(clone_dir)
+        origin = repo.remotes.origin
+        origin.fetch()
+        repo.git.reset('--hard', 'origin/master')
 
-# Build Ethereum app for all devices
-# test if the directory already exists
-if not os.path.exists("lib_binaries"):
-    if not os.path.exists(APP_ETHEREUM_DIR):
-        Repo.clone_from(APP_ETHEREUM_URL, APP_ETHEREUM_DIR, multi_options=["--recurse-submodules"])
-    os.makedirs("lib_binaries", exist_ok=True)
-    flags = ' COIN=ethereum CHAIN=ethereum CAL_TEST_KEY=1 DOMAIN_NAME_TEST_KEY=1 SET_PLUGIN_TEST_KEY=1 NFT_TEST_KEY=1 TRUSTED_NAME_TEST_KEY=1'
-    run_cmd("make clean", Path(APP_ETHEREUM_DIR))
-    for e in DEVICES_CONF.values():
-        run_cmd("make -j BOLOS_SDK=$"+e["sdk"]+flags, Path(APP_ETHEREUM_DIR))
-        run_cmd("cp "+e["bin_path"]+"app.elf ../lib_binaries/ethereum_"+e["name"]+".elf", Path(APP_ETHEREUM_DIR))
-    run_cmd("rm -rf "+APP_ETHEREUM_DIR)
+        # Update submodules
+        print(f"Updating submodules in {clone_dir}")
+        run_cmd("git submodule sync", cwd=Path(clone_dir))
+        run_cmd("git submodule update --init --recursive", cwd=Path(clone_dir))
+
+def build_app(clone_dir: str, flags: str):
+    for d in DEVICES_CONF.values():
+        sdk = d["sdk"]
+        cmd = f"make -j BOLOS_SDK=${sdk} {flags}"
+        run_cmd(cmd, cwd=Path(clone_dir))
+
+def copy_build_output(clone_dir: str, dest_dir: str):
+    run_cmd(f"mkdir -p {dest_dir}")
+    run_cmd(f"cp -rT {clone_dir}/build {dest_dir}/build")
+
+
+# ==== Build app-exchange ====
+clone_or_pull(APP_EXCHANGE_URL, APP_EXCHANGE_CLONE_DIR)
+build_app(APP_EXCHANGE_CLONE_DIR, flags="TESTING=1 TEST_PUBLIC_KEY=1 TRUSTED_NAME_TEST_KEY=1 DEBUG=1")
+copy_build_output(APP_EXCHANGE_CLONE_DIR, APP_EXCHANGE_DIR)
+
+# ==== Build app-ethereum ====
+clone_or_pull(APP_ETHEREUM_URL, APP_ETHEREUM_CLONE_DIR)
+build_app(APP_ETHEREUM_CLONE_DIR, flags="COIN=ethereum CHAIN=ethereum CAL_TEST_KEY=1 DOMAIN_NAME_TEST_KEY=1 SET_PLUGIN_TEST_KEY=1 NFT_TEST_KEY=1 TRUSTED_NAME_TEST_KEY=1")
+copy_build_output(APP_ETHEREUM_CLONE_DIR, APP_ETHEREUM_DIR)
