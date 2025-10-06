5e85d7673e835a53e312c4fb6cea42bffe4d0226
Gustavo Jodar <gustavo.jodar-soares@ledger.fr>
2025-10-06 14:18:04 +0200

    Update clusterFuzz

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,8 +2,26 @@
 
 # build fuzzers
 
+export BOLOS_SDK=/ledger-secure-sdk
+
 pushd fuzzing
-cmake -DBOLOS_SDK=../BOLOS_SDK -Bbuild -H.
-make -C build
-mv ./build/fuzz_tx_parser "${OUT}"
+cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug \
+            -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
+            -DBOLOS_SDK=${BOLOS_SDK} -DTARGET=flex \
+            -DAPP_BUILD_PATH=/app
+
+# Generates .zip for initial corpus in clusterFuzz
+for dir in harness/*; do
+    if [ -d "$dir" ]; then
+        fuzzer_name=$(basename "$dir")
+        zip_name="${fuzzer_name}_seed_corpus.zip"
+        echo "Zipping corpus from $dir into $zip_name"
+
+        (cd "$dir" && zip -q -r "$zip_name" .)
+
+        mv "$dir/$zip_name" "$OUT/"
+    fi
+done
+cmake --build build
+mv ./build/fuzz_* "${OUT}"
 popd
