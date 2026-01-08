ce5ac740467fad377075c10814f74688a7dc004e
Jan Mazak <jan.mazak@vacuumlabs.com>
2026-01-08 17:50:34 +0100

    remove ui code from hash computation

--- src/transaction/tx_prepare.c
+++ src/transaction/tx_prepare.c
@@ -23,7 +23,6 @@
 #include "io.h"
 #include "app_context.h"
 #include "utils/cbor.h"
-#include "ui/tx_ui_helpers.h"
 
 #define UI_PAIR_LIMIT 250
 
@@ -1002,19 +1001,6 @@ int compute_tx_hash_and_plan_ui(tx_ui_plan_t* plan) {
             }
 
             uint8_t reward_address[REWARD_ACCOUNT_LENGTH];
-            char temp_buf[256];  // Temporary buffer for helper function
-
-            // Use helper to construct reward address
-            bool addr_constructed = formatRewardAddressFromCredential(
-                G_context.tx_info.transaction.networkId,
-                &withdrawal_item->withdrawal_data.stakeCredential,
-                temp_buf,
-                sizeof(temp_buf)
-            );
-            LEDGER_ASSERT(addr_constructed, "Failed to format withdrawal reward address");
-
-            // For hash builder, we need the raw reward address bytes
-            // Re-construct using the direct functions (helper only gives formatted string)
             size_t reward_addr_len = 0;
             switch (withdrawal_item->withdrawal_data.stakeCredential.type) {
                 case EXT_CREDENTIAL_KEY_PATH:
