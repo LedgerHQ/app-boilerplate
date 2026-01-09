54db678b72b2abb9864ff620295a2b63a465eecc
Jan Mazak <jan.mazak@vacuumlabs.com>
2026-01-09 22:20:27 +0100

    comment out fixed seed

--- tests/standalone/conftest.py
+++ tests/standalone/conftest.py
@@ -10,7 +10,7 @@ from ragger.conftest import configuration
 # Ragger tests are supposed to run without any hardcoded seed / mnemonic.
 # However, for debugging, we might want to fix the seed occasionally
 # to a value corresponding to the unit test fixtures.
-configuration.OPTIONAL.CUSTOM_SEED = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+#configuration.OPTIONAL.CUSTOM_SEED = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
 
 #########################
 ### CONFIGURATION END ###
