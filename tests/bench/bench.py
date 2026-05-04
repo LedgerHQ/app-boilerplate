d7712d06e85a702b49dc108550bdf64c8f6d4f63
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-05-04 16:44:33 +0200

    Bench script now checks the computed prime number

--- tests/bench/bench.py
+++ tests/bench/bench.py
@@ -1,20 +1,36 @@
 import struct
 import time
+from enum import IntEnum
 
+import sympy
 from ledgercomm import Transport
 
-# trans = Transport(interface="tcp", server="127.0.0.1", port=9999, debug=False)
-trans = Transport(interface="hid", debug=False)
+# speculos
+trans = Transport(interface="tcp", server="127.0.0.1", port=9999, debug=False)
+# physical device
+#trans = Transport(interface="hid", debug=False)
 
-times = []
+class Ins(IntEnum):
+    BENCH_PRIME = 0x01
+    BENCH_FIBO = 0x02
 
-for _ in range(5):
-    before = time.time_ns()
-    trans.send(cla=0xe0, ins=0x01, p1=0x00, p2=0x00, cdata=struct.pack(">I", 100000))
-    trans.recv()
-    after = time.time_ns()
-    times.append(after - before)
-    print("[%u] %u" % (len(times) - 1, times[-1]))
 
-avg = sum(times) // len(times)
-print("=> avg of %u ns" % (avg))
+def bench_prime() -> None:
+    times = []
+    max_count = 100000
+    ref_value = sympy.prime(max_count)
+
+    for _ in range(5):
+        before = time.time_ns()
+        trans.send(cla=0xe0, ins=Ins.BENCH_PRIME, p1=0x00, p2=0x00, cdata=struct.pack(">I", 100000))
+        _, res = trans.recv()
+        value = struct.unpack(">I", res)[0]
+        assert value == ref_value
+        after = time.time_ns()
+        times.append(after - before)
+        print("[%u] %u" % (len(times) - 1, times[-1]))
+
+    avg = sum(times) // len(times)
+    print("=> avg of %u ns" % (avg))
+
+bench_prime()
