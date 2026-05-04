04e62cd0d685f861b95b10a37412c09ac5c0fa30
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-05-04 17:29:26 +0200

    App now returns the last computed prime number

--- src/apdu/dispatcher.c
+++ src/apdu/dispatcher.c
@@ -28,6 +28,7 @@
 #include "sw.h"
 #include "bench.h"
 #include "read.h"
+#include "write.h"
 
 int apdu_dispatcher(const command_t *cmd) {
     LEDGER_ASSERT(cmd != NULL, "NULL cmd");
@@ -36,13 +37,17 @@ int apdu_dispatcher(const command_t *cmd) {
         return io_send_sw(SWO_INVALID_CLA);
     }
 
+    uint32_t value;
+    uint8_t buf[sizeof(value)];
+
     switch (cmd->ins) {
         case BENCH_PRIME:
             if (cmd->lc != sizeof(uint32_t)) {
                 return io_send_sw(SWO_WRONG_DATA_LENGTH);
             }
-            bench_prime(read_u32_be(cmd->data, 0));
-            return io_send_sw(SWO_SUCCESS);
+            value = bench_prime(read_u32_be(cmd->data, 0));
+            write_u32_be(buf, 0, value);
+            return io_send_response_pointer(buf, sizeof(buf), SWO_SUCCESS);
 
         case BENCH_FIBO:
             if (cmd->lc != sizeof(uint32_t)) {
